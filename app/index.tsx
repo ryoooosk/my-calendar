c94758c1c9e0e9dc06de6bf655c85d2305f5ed0d
ryoooosk <pond.well.0205@gmail.com>
2024-12-22 14:16:54 +0900

    feat: トップページにカレンダーを静的表示

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,68 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { CalendarList, DateData, LocaleConfig } from 'react-native-calendars';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Calender() {
+  const [selected, setSelected] = useState('');
+
   return (
     <SafeAreaView>
-      <Text>Calendar</Text>
+      <CalendarList
+        monthFormat="yyyy年 M月"
+        pagingEnabled={true}
+        horizontal={true}
+        onDayPress={(day: DateData) => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+          },
+        }}
+      />
     </SafeAreaView>
   );
 }
+
+LocaleConfig.locales.jp = {
+  monthNames: [
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月',
+  ],
+  monthNamesShort: [
+    '1月',
+    '2月',
+    '3月',
+    '4月',
+    '5月',
+    '6月',
+    '7月',
+    '8月',
+    '9月',
+    '10月',
+    '11月',
+    '12月',
+  ],
+  dayNames: [
+    '日曜日',
+    '月曜日',
+    '火曜日',
+    '水曜日',
+    '木曜日',
+    '金曜日',
+    '土曜日',
+  ],
+  dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+};
+LocaleConfig.defaultLocale = 'jp';
