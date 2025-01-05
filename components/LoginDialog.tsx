ce1d4b406932d0ee2365114a58dbcd94668f4e3d
ryoooosk <pond.well.0205@gmail.com>
2025-01-05 22:08:39 +0900

    feat: appleでサインインの仮実装

--- components/LoginDialog.tsx
+++ components/LoginDialog.tsx
@@ -1,4 +1,8 @@
+import { supabase } from '@/lib/supabase';
+import * as AppleAuthentication from 'expo-apple-authentication';
 import { Link } from 'expo-router';
+import { Platform } from 'react-native';
+import AppleSignInButton from './AppleSignInButton';
 import LoginForm from './LoginForm';
 import {
   AlertDialog,
@@ -16,6 +20,11 @@ type LoginDialogProps = {
 };
 
 export default function LoginDialog({ isShow, setIsShow }: LoginDialogProps) {
+  const appleSignInHandle = () => {
+    setIsShow(false);
+    appleSignIn();
+  };
+
   return (
     <AlertDialog isOpen={isShow} onClose={() => setIsShow(false)} size="md">
       <AlertDialogBackdrop />
@@ -30,6 +39,10 @@ export default function LoginDialog({ isShow, setIsShow }: LoginDialogProps) {
           <LoginForm setIsShow={setIsShow} />
         </AlertDialogBody>
 
+        {Platform.OS === 'ios' && (
+          <AppleSignInButton signInHandle={appleSignInHandle} />
+        )}
+
         <AlertDialogFooter className="flex justify-center mt-2">
           <Link href="/signup" onPress={() => setIsShow(false)}>
             アカウント作成はこちら
@@ -39,3 +52,37 @@ export default function LoginDialog({ isShow, setIsShow }: LoginDialogProps) {
     </AlertDialog>
   );
 }
+
+const appleSignIn = async () => {
+  try {
+    const credential = await AppleAuthentication.signInAsync({
+      requestedScopes: [
+        AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+        AppleAuthentication.AppleAuthenticationScope.EMAIL,
+      ],
+    });
+    if (!credential.identityToken) throw new Error('No identityToken.');
+
+    // supabaseのAppleプロパイダーのclientIdにApple Developer Consoleで作成したApp Idを登録必要
+    const {
+      error,
+      data: { user },
+    } = await supabase.auth.signInWithIdToken({
+      provider: 'apple',
+      token: credential.identityToken,
+    });
+    console.log('Supabase Response:', { error, user });
+
+    if (!error) {
+      // User is signed in.)
+    }
+  } catch (e) {
+    if (e.code === 'ERR_REQUEST_CANCELED') {
+      // handle that the user canceled the sign-in flow
+      console.error('error code', e);
+    } else {
+      // handle other errors
+      console.error('error', e);
+    }
+  }
+};
