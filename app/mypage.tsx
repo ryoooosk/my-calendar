416930b81d10a7da23485e46c74368b4cc2c8636
ryoooosk <pond.well.0205@gmail.com>
2025-02-02 00:14:10 +0900

    refactor: ユーザー取得をhooks化

--- app/mypage.tsx
+++ app/mypage.tsx
@@ -1,62 +1,30 @@
-import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { Avatar } from '@/components/ui/avatar';
 import { Button, ButtonIcon, ButtonText } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { HStack } from '@/components/ui/hstack';
 import { Icon } from '@/components/ui/icon';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
-import { Users } from '@/database.types';
+import { useAuth } from '@/hooks/useAuth';
 import { supabase } from '@/lib/supabase';
-import { useNavigation } from 'expo-router';
+import { useRouter } from 'expo-router';
 import { LogOut, Pencil, Share, User as UserIcon } from 'lucide-react-native';
-import { useEffect, useState } from 'react';
 import { View } from 'react-native';
 
 export default function MyPage() {
-  const navigation = useNavigation();
-  const [user, setUser] = useState<Users | null>(null);
-  const [avatarUrl, setAvatarUrl] = useState<string | undefined>(undefined);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      if (!session) return;
-
-      const { data: user, error } = await supabase
-        .from('users')
-        .select('*')
-        .eq('id', session.user.id)
-        .single();
-
-      if (error) console.error(error);
-      else setUser(user);
-
-      if (!user?.avatar_url) return;
-      const { data: avatarData } = supabase.storage
-        .from('avatars')
-        .getPublicUrl(user?.avatar_url);
-      if (avatarData) setAvatarUrl(avatarData.publicUrl);
-    };
-
-    fetchData();
-  }, []);
+  const router = useRouter();
+  const { user } = useAuth();
 
   const logOutHandle = () => {
     supabase.auth.signOut();
-    navigation.navigate('index');
+    router.replace('/');
   };
 
   return (
     <VStack space="xl" className="mt-6 px-3">
       <HStack space="lg" className="">
         <Avatar size="xl" className="bg-slate-600">
-          {avatarUrl ? (
-            <AvatarImage source={{ uri: avatarUrl }} />
-          ) : (
-            <Icon as={UserIcon} size="xl" className="stroke-white" />
-          )}
+          <Icon as={UserIcon} size="xl" className="stroke-white" />
         </Avatar>
 
         <VStack className="flex gap-1">
