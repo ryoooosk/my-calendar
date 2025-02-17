02d20fd4e839143f10f3203ba652e5c47def103f
ryoooosk <pond.well.0205@gmail.com>
2025-02-17 20:43:15 +0900

    feat(components/pages/schedules/schedule-item): 編集ボタン設置

--- components/pages/schedules/schedule-item/index.tsx
+++ components/pages/schedules/schedule-item/index.tsx
@@ -1,4 +1,6 @@
-import { Text, TouchableOpacity } from 'react-native';
+import { Icon } from '@/components/ui/icon';
+import { EditIcon } from 'lucide-react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { AgendaEntry } from 'react-native-calendars';
 
 export default function ScheduleItem({
@@ -9,31 +11,36 @@ export default function ScheduleItem({
   isFirstInDay: boolean;
 }) {
   return (
-    <TouchableOpacity
-      key={schedule.name}
-      className={`flex-1 flex gap-1 bg-white px-4 py-3 mr-5 mt-4 rounded-xl ${!isFirstInDay && 'ml-16'}`}
+    <View
+      className={`flex-1 flex flex-row items-center bg-white px-4 py-3 mr-5 mt-4 rounded-xl ${!isFirstInDay && 'ml-16'}`}
     >
-      <Text
-        className="text-xl font-medium tracking-wider text-slate-800"
-        numberOfLines={1}
-        ellipsizeMode="tail"
-      >
-        {schedule.name}
-      </Text>
-
-      <Text className="text-[1.13rem] tracking-wide text-slate-800">
-        {schedule.isAllDay ? '終日' : schedule.day}
-      </Text>
-
-      {schedule.description && (
+      <View className="flex-1 flex gap-1">
         <Text
-          className="mt-2 text-gray-500 tracking-wide"
-          numberOfLines={3}
+          className="text-xl font-medium tracking-wider text-slate-800"
+          numberOfLines={1}
           ellipsizeMode="tail"
         >
-          {schedule.description}
+          {schedule.name}
         </Text>
-      )}
-    </TouchableOpacity>
+
+        <Text className="text-[1.13rem] tracking-wide text-slate-800">
+          {schedule.isAllDay ? '終日' : schedule.day}
+        </Text>
+
+        {schedule.description && (
+          <Text
+            className="mt-2 text-gray-500 tracking-wide"
+            numberOfLines={3}
+            ellipsizeMode="tail"
+          >
+            {schedule.description}
+          </Text>
+        )}
+      </View>
+
+      <TouchableOpacity className="p-2">
+        <Icon as={EditIcon} className="w-8 h-8" />
+      </TouchableOpacity>
+    </View>
   );
 }
