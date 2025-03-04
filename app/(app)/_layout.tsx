51d514b03a134511d2880b7242187cdf288b7825
ryoooosk <pond.well.0205@gmail.com>
2025-03-04 22:35:24 +0900

    refactor(app/(app)/_layout): コード整理

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -8,32 +8,33 @@ import LoadingScreen from '../loading-screen';
 export default function AppLayout() {
   const { session, isLoading } = useContext(AuthContext);
 
-  if (isLoading && !session) {
-    return <LoadingScreen />;
-  }
-  if (!session) {
-    return <Redirect href="/login" />;
-  }
-
+  if (!session && !isLoading) return <Redirect href="/login" />;
   return (
     <NotificationSubscriptionProvider>
-      <ScheduleProvider>
-        <Stack>
-          <Stack.Screen
-            name="(calendar)"
-            options={{ headerShown: false, title: 'ホーム' }}
-          />
-          <Stack.Screen name="mypage/index" options={{ title: 'マイページ' }} />
-          <Stack.Screen
-            name="mypage/edit"
-            options={{
-              presentation: 'modal',
-              title: 'プロフィール編集',
-              headerBackTitle: 'キャンセル',
-            }}
-          />
-        </Stack>
-      </ScheduleProvider>
+      {session ? (
+        <ScheduleProvider>
+          <Stack>
+            <Stack.Screen
+              name="(calendar)"
+              options={{ headerShown: false, title: 'ホーム' }}
+            />
+            <Stack.Screen
+              name="mypage/index"
+              options={{ title: 'マイページ' }}
+            />
+            <Stack.Screen
+              name="mypage/edit"
+              options={{
+                presentation: 'modal',
+                title: 'プロフィール編集',
+                headerBackTitle: 'キャンセル',
+              }}
+            />
+          </Stack>
+        </ScheduleProvider>
+      ) : (
+        <LoadingScreen />
+      )}
     </NotificationSubscriptionProvider>
   );
 }
