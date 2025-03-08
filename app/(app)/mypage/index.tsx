b0e8b052d9995da8bb033ecc17474d3f439f1f9b
ryoooosk <pond.well.0205@gmail.com>
2025-03-08 22:39:44 +0900

    style(app/(app)/mypage): 白背景に

--- app/(app)/mypage/index.tsx
+++ app/(app)/mypage/index.tsx
@@ -2,10 +2,15 @@ import MyPageContainer from '@/components/pages/Mypage';
 import { Spinner } from '@/components/ui/spinner';
 import { AuthContext } from '@/contexts/AuthContext';
 import { useContext } from 'react';
+import { View } from 'react-native';
 
 export default function MyPage() {
   const { user } = useContext(AuthContext);
 
   if (!user) return <Spinner />;
-  return <MyPageContainer user={user} />;
+  return (
+    <View className="bg-white">
+      <MyPageContainer user={user} />
+    </View>
+  );
 }
