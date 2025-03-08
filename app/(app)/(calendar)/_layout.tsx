389bf312811233303f9cb12e5662b8e268126b37
ryoooosk <pond.well.0205@gmail.com>
2025-03-08 23:46:29 +0900

    feat((app)/(calendar)/_layout): 新規作成ボタンを共有化

--- app/(app)/(calendar)/_layout.tsx
+++ app/(app)/(calendar)/_layout.tsx
@@ -1,19 +1,35 @@
+import { Button, ButtonIcon } from '@/components/ui/button';
 import { DateProvider } from '@/contexts/DateContext';
-import { Stack } from 'expo-router';
+import { Stack, router } from 'expo-router';
+import { PlusIcon } from 'lucide-react-native';
+import { View } from 'react-native';
 
 export default function CalendarLayout() {
+  const handleClick = () => {
+    router.push('/schedule/create');
+  };
+
   return (
     <DateProvider>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{ headerShown: false, title: 'ホーム' }}
-        />
-        <Stack.Screen
-          name="timeline/[date]"
-          options={{ title: 'タイムライン' }}
-        />
-      </Stack>
+      <View className="relative flex-1">
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{ headerShown: false, title: 'ホーム' }}
+          />
+          <Stack.Screen
+            name="timeline/[date]"
+            options={{ title: 'タイムライン' }}
+          />
+        </Stack>
+
+        <Button
+          className="absolute right-5 bottom-12 w-16 h-16 rounded-full"
+          onPress={handleClick}
+        >
+          <ButtonIcon className="w-11 h-11" as={PlusIcon} />
+        </Button>
+      </View>
     </DateProvider>
   );
 }
